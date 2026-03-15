08a9615baf7d8f50a30e9ae6046cfebd09aaa21d
progkiryu <denmesa24@outlook.com>
2026-03-15 13:11:49 +1100

    add dotenv to python server, add ngrok hosting on python server and change client's prod address

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './main.css'
 
-export const DBLink = import.meta.env.VITE_DEV_ADDR
+export const DBLink = import.meta.env.MODE === "development" ? 
+  import.meta.env.VITE_DEV_ADDR : import.meta.env.VITE_PROD_ADDR;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
